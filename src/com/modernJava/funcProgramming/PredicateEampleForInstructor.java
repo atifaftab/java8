e25d7da02ca2c5a863f6090816684ac6311a5f19
Atif <iamatif96@gmail.com>
2024-02-10 17:19:32 +0400

    Predicate for instructor class

---
+++ src/com/modernJava/funcProgramming/PredicateEampleForInstructor.java
@@ -0,0 +1,30 @@
+package com.modernJava.funcProgramming;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class PredicateEampleForInstructor {
+    public static void main(String[] args) {
+        List<Instructor> instructorList = Instructors.getAll();
+
+        //all instructor who teaches online
+        Predicate<Instructor> predicate = i -> i.isOnlineCourse();
+//        Predicate<Instructor> predicate = Instructor::isOnlineCourse;
+        //instructor experience >10
+        Predicate<Instructor> predicate1 = i -> i.getYearsOfExperience() > 10;
+
+        instructorList.forEach(i -> {
+            if (predicate.test(i))
+                System.out.println(i);
+        });
+
+        System.out.println("---------------");
+
+        //is instructor teaches online and experience is >10 years
+        instructorList.forEach(i -> {
+            if (predicate1.and(predicate).test(i)) {
+                System.out.println(i);
+            }
+        });
+    }
+}
