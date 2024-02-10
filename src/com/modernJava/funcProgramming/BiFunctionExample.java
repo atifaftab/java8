0158e940f74512ea55cdde6dc1857df70fda08e1
Atif <iamatif96@gmail.com>
2024-02-10 21:35:23 +0400

    BiFunction example with instructor

---
+++ src/com/modernJava/funcProgramming/BiFunctionExample.java
@@ -0,0 +1,32 @@
+package com.modernJava.funcProgramming;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Predicate;
+
+public class BiFunctionExample {
+    public static void main(String[] args) {
+        //BiFunction 2 inputs List<Instructor> and 2nd is predicate which will filter if the instructor has online
+        //courses and return a map of <String,Integer> String is name and Integer is years of experience
+
+//        Predicate<Instructor> instructorPredicate = i -> i.isOnlineCourse();
+        Predicate<Instructor> instructorPredicate = Instructor::isOnlineCourse;
+
+        List<Instructor> instructorList = Instructors.getAll();
+
+        BiFunction<List<Instructor>, Predicate<Instructor>, Map<String, Integer>> biFunction = (instructors, predicate) -> {
+            Map<String, Integer> map = new HashMap<>();
+            instructorList.forEach(instructor -> {
+                if (predicate.test(instructor)) {
+                    map.put(instructor.getName(), instructor.getYearsOfExperience());
+                }
+            });
+            return map;
+        };
+//        instructorList.forEach(i-> biFunction.apply(i,instructorPredicate));
+        System.out.println(biFunction.apply(instructorList, instructorPredicate));
+
+    }
+}
