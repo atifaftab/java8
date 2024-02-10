69772eef0096fa6fdcc0a8c847a89e9563ecc131
Atif <iamatif96@gmail.com>
2024-02-10 20:55:09 +0400

    Function example with instructor

---
+++ src/com/modernJava/funcProgramming/FunctionExampleForInsructor.java
@@ -0,0 +1,35 @@
+package com.modernJava.funcProgramming;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class FunctionExampleForInsructor {
+    public static void main(String[] args) {
+        //Map of instructor with name and years of experience
+        //Function which will tale List<Instructor> and return Map<String, Integer>
+        //to create map of instructor who have online courses, so create predicate
+
+        List<Instructor> instructorList = Instructors.getAll();
+
+        Predicate<Instructor> instructorPredicate = Instructor::isOnlineCourse;
+//        Predicate<Instructor> instructorPredicate = Instructor::isOnlineCourse;
+
+        Function<List<Instructor>, Map<String, Integer>> mapFunction = instructors -> {
+            Map<String, Integer> mapNameAndExperience = new HashMap<>();
+            instructors.forEach(instructor -> {
+                if (instructorPredicate.test(instructor)) {
+                    mapNameAndExperience.put(instructor.getName(), instructor.getYearsOfExperience());
+                }
+            });
+            return mapNameAndExperience;
+        };
+        System.out.println(mapFunction.apply(instructorList));
+//        System.out.println("---------------");
+//
+//        Map<String, Integer> stringIntegerMap = mapFunction.apply(instructorList);
+//        stringIntegerMap.forEach((k, v) -> System.out.println(k + " " + v));
+    }
+}
