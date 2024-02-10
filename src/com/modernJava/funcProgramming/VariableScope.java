ebdb85e2dc9d30f199358ea692711eb972eac6bd
Atif <iamatif96@gmail.com>
2024-02-11 02:17:19 +0400

    Variable scope : effectively final

---
+++ src/com/modernJava/funcProgramming/VariableScope.java
@@ -0,0 +1,10 @@
+package com.modernJava.funcProgramming;
+
+public class VariableScope {
+    public static void main(String[] args) {
+        // Effectively final
+        // do not modify local variable even they are not declare final
+        //once local variable used in lambda do not change it after
+        //its called effectively final
+    }
+}
