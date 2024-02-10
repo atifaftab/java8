37f7497c063efe3f8f0f9bea5528b3d298a179c4
Atif <iamatif96@gmail.com>
2024-02-10 16:36:14 +0400

    Predicate

---
+++ src/com/modernJava/funcProgramming/PredicateExample.java
@@ -0,0 +1,29 @@
+package com.modernJava.funcProgramming;
+
+import java.util.function.Predicate;
+
+public class PredicateExample {
+    public static void main(String[] args) {
+
+        //if number>10 return tre or false
+        Predicate<Integer> predicate = i -> i>10;
+        System.out.println(predicate.test(14));
+        System.out.println(predicate.test(5));
+
+        System.out.println("---------------");
+
+        //i>10 && number is even number (i%2==0)
+        Predicate<Integer> predicate1 = i -> i%2==0;
+        System.out.println(predicate.and(predicate1).test(16));
+
+        System.out.println("---------------");
+
+        //i>10 || number is even number (i%2==0)
+        System.out.println(predicate.or(predicate1).test(14));
+
+        System.out.println("---------------");
+
+        //i>10 && number is odd (i%2!=0)
+        System.out.println(predicate.and(predicate1.negate()).test(12));
+    }
+}
