b0e1f9cfaa15e98df64421af12d6470a2b0def5a
Atif <iamatif96@gmail.com>
2024-02-12 01:01:00 +0400

    sort by Comparator

---
+++ src/com/modernJava/streams/StreamComparatorExample.java
@@ -0,0 +1,31 @@
+package com.modernJava.streams;
+
+import com.modernJava.funcProgramming.Instructor;
+import com.modernJava.funcProgramming.Instructors;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class StreamComparatorExample {
+    public static void main(String[] args) {
+
+        //returning all instructor sorted by their name
+        List<Instructor> instructorList = Instructors.getAll().stream()
+                .sorted(Comparator.comparing(Instructor::getName))
+                .toList();
+
+        System.out.println(instructorList);
+        System.out.println("---------------");
+        instructorList.forEach(System.out::println);
+
+        System.out.println("---------------");
+        
+
+        //reversed
+        List<Instructor> instructorReversedList = Instructors.getAll().stream()
+                .sorted(Comparator.comparing(Instructor::getName).reversed())
+                .toList();
+
+        instructorReversedList.forEach(System.out::println);
+    }
+}
