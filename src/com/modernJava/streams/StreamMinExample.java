21cd2bc947aa4db17fe5f2e2815d597b31eae0ad
Atif <iamatif96@gmail.com>
2024-02-12 08:03:32 +0400

    Min of stream and also reduce

---
+++ src/com/modernJava/streams/StreamMinExample.java
@@ -0,0 +1,36 @@
+package com.modernJava.streams;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+public class StreamMinExample {
+    public static void main(String[] args) {
+        //min using min of Stream
+        List<Integer> number = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        //using min
+        Optional<Integer> min = number.stream().min(Integer::compareTo);
+        if (min.isPresent()) {
+            System.out.println(min.get());
+        }
+        //same
+        min.ifPresent(System.out::println);
+
+        //REDUCE
+
+        //using identity and binaryOperation
+        int reduceMin = number.stream().reduce(0, (a, b) -> a < b ? a : b);
+        System.out.println(reduceMin);
+        //only binaryOperation
+        Optional<Integer> reduceMinBinary = number.stream().reduce((a, b) -> a < b ? a : b);
+        reduceMinBinary.ifPresent(System.out::println);
+
+        //USING MIN AND MAX
+        Optional<Integer> max = number.stream().reduce(Integer::max);
+        max.ifPresent(System.out::println);
+
+        Optional<Integer> min1 = number.stream().reduce(Integer::min);
+        min1.ifPresent(System.out::println);
+    }
+}
