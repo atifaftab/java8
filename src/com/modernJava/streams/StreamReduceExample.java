c55cafbfde65e652e5831aea7fb262e4114ecb45
Atif <iamatif96@gmail.com>
2024-02-12 07:06:21 +0400

    Reduce Example

---
+++ src/com/modernJava/streams/StreamReduceExample.java
@@ -0,0 +1,33 @@
+package com.modernJava.streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class StreamReduceExample {
+    public static void main(String[] args) {
+        List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+//        List<Integer> integerList = Arrays.asList();
+        int sum = integerList.stream()
+                //0 +1=1    //10+5=15       //36+9 = 45
+                //1+2=3     //15+6 = 21
+                //3+3=6     //21+7 = 28
+                //6+5=10    //28+8 = 36
+                .reduce(0, Integer::sum);
+        System.out.println(sum);
+
+        int multiplication = integerList.stream()
+                .reduce(1, (a, b) -> a * b);
+        System.out.println(multiplication);
+
+        //using reduce but 2nd type
+        Optional<Integer> optional = integerList.stream()
+                .reduce(Integer::sum);
+
+        optional.ifPresent(System.out::println);
+
+//        if (optional.isPresent()) {
+//            System.out.println(optional.get());
+//        }
+    }
+}
