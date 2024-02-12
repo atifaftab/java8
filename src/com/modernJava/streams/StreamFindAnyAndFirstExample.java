b450bf0af53955cea8c6f8cccdcd264decdc4518
Atif <iamatif96@gmail.com>
2024-02-12 08:17:14 +0400

    find any and find first f stream

---
+++ src/com/modernJava/streams/StreamFindAnyAndFirstExample.java
@@ -0,0 +1,20 @@
+package com.modernJava.streams;
+
+import com.modernJava.funcProgramming.Instructor;
+import com.modernJava.funcProgramming.Instructors;
+
+import java.util.Optional;
+
+public class StreamFindAnyAndFirstExample {
+
+    public static void main(String[] args) {
+
+        //findFirst
+        Optional<Instructor> first = Instructors.getAll().stream().findFirst();
+        first.ifPresent(System.out::println);
+
+        //findAny
+        Optional<Instructor> any = Instructors.getAll().stream().findAny();
+        any.ifPresent(System.out::println);
+    }
+}
