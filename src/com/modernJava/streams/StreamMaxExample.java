af3ae1cb7627ca6ec524385bf8557ba5e10442a9
Atif <iamatif96@gmail.com>
2024-02-12 07:46:48 +0400

    Stream Max example

---
+++ src/com/modernJava/streams/StreamMaxExample.java
@@ -0,0 +1,29 @@
+package com.modernJava.streams;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+public class StreamMaxExample {
+    public static void main(String[] args) {
+        // max using stream through reduce
+
+        List<Integer> numberList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        //max using stream max function
+        Optional optional = numberList.stream().max(Integer::compareTo);
+        if (optional.isPresent()) {
+            System.out.println(optional.get());
+        }
+        //same
+        Optional<Integer> max = numberList.stream().max(Integer::compareTo);
+        max.ifPresent(System.out::println);
+
+        Optional<Integer> reduce = numberList.stream().reduce((a, b) -> a > b ? a : b);
+        reduce.ifPresent(System.out::println);
+
+        Optional<Integer> sum = numberList.stream().reduce(Integer::sum);
+        sum.ifPresent(System.out::println);
+
+    }
+}
