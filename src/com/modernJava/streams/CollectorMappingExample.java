ef9c19efbdb013da1d4865dc26b64a585ffa6afc
Atif <iamatif96@gmail.com>
2024-02-13 19:01:23 +0400

    using Collectors.mapping()

---
+++ src/com/modernJava/streams/CollectorMappingExample.java
@@ -0,0 +1,28 @@
+package com.modernJava.streams;
+
+import com.modernJava.funcProgramming.Instructor;
+import com.modernJava.funcProgramming.Instructors;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CollectorMappingExample {
+    public static void main(String[] args) {
+        List<String> namesList = Instructors.getAll().stream()
+                .map(Instructor::getName).toList();
+        namesList.forEach(System.out::println);
+
+        System.out.println("-----");
+        namesList = Instructors.getAll().stream()
+                .collect(Collectors.mapping(Instructor::getName, Collectors.toList()));
+
+        namesList.forEach(System.out::println);
+
+        //Instructor By years of experience with name
+        Map<Integer, List<String>> mapOfExperienceAndNames = Instructors.getAll().stream()
+                .collect(Collectors.groupingBy(Instructor::getYearsOfExperience,
+                        Collectors.mapping(Instructor::getName, Collectors.toList())));
+        mapOfExperienceAndNames.forEach((k, v) -> System.out.println("key " + k + " value " + v));
+    }
+}
