aa2cd5ed59e8910421f712c8315d9e7c74884b9e
Atif <iamatif96@gmail.com>
2024-02-15 22:01:42 +0400

    using Collectors summingInt and averaging Int

---
+++ src/com/modernJava/streams/CollectorsSummingAveragingExample.java
@@ -0,0 +1,20 @@
+package com.modernJava.streams;
+
+import com.modernJava.funcProgramming.Instructor;
+import com.modernJava.funcProgramming.Instructors;
+
+import java.util.stream.Collectors;
+
+public class CollectorsSummingAveragingExample {
+    public static void main(String[] args) {
+
+        int sum = Instructors.getAll().stream()
+                .collect(Collectors.summingInt(Instructor::getYearsOfExperience));
+//                .mapToInt(Instructor::getYearsOfExperience).sum();
+        System.out.println("sum = " + sum);
+
+        double average = Instructors.getAll().stream()
+                .collect(Collectors.averagingInt(Instructor::getYearsOfExperience));
+        System.out.println("average = " + average);
+    }
+}
