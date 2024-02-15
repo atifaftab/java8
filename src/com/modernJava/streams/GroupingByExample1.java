0089ab3cfc0ac2778fe658e56557713a44f92ab2
Atif <iamatif96@gmail.com>
2024-02-15 22:22:42 +0400

    using Collectors groupingBy for Instructor

--- src/com/modernJava/streams/GroupingByExample1.java
+++ src/com/modernJava/streams/GroupingByExample1.java
@@ -3,7 +3,6 @@ package com.modernJava.streams;
 import com.modernJava.funcProgramming.Instructor;
 import com.modernJava.funcProgramming.Instructors;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -20,15 +19,11 @@ public class GroupingByExample1 {
         Map<String, List<Instructor>> mapGenderInstructor = Instructors.getAll().stream()
                 .collect(Collectors.groupingBy(Instructor::getGender));
 //        System.out.println("mapGenderInstructor = " + mapGenderInstructor);
-        mapGenderInstructor.forEach((k, v) -> {
-            System.out.println("key = " + k + " value = " + v);
-        });
+        mapGenderInstructor.forEach((k, v) -> System.out.println("key = " + k + " value = " + v));
         System.out.println("-----");
         // grouping by experience where >10 years of experience is classified as senior and other are juniors
         Map<String, List<Instructor>> mapExperienceInstructor = Instructors.getAll().stream()
                 .collect(Collectors.groupingBy(i -> i.getYearsOfExperience() > 10 ? "SENIOR" : "JUNIOR"));
-        mapExperienceInstructor.forEach((k, v) -> {
-            System.out.println("key = " + k + " value = " + v);
-        });
+        mapExperienceInstructor.forEach((k, v) -> System.out.println("key = " + k + " value = " + v));
     }
 }
