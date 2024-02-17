915dd576129c4038888fb00d5b1c7a321b92ee67
Atif <iamatif96@gmail.com>
2024-02-17 18:10:52 +0400

    using Collectors groupingBy for Instructor for 3 param and LinkedHashMap

--- src/com/modernJava/streams/GroupingExample3.java
+++ src/com/modernJava/streams/GroupingExample3.java
@@ -3,8 +3,8 @@ package com.modernJava.streams;
 import com.modernJava.funcProgramming.Instructor;
 import com.modernJava.funcProgramming.Instructors;
 
+import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class GroupingExample3 {
@@ -13,16 +13,16 @@ public class GroupingExample3 {
         //grouping by length of String and also checking that the names contains e and only return those names
         // which has e in it
         List<String> names = List.of("Sid", "Mike", "Jenny", "Gene", "Rajeev");
-        Map<Integer, List<String>> namesMap = names.stream()
-                .collect(Collectors.groupingBy(String::length, Collectors.filtering(s -> s.contains("e"), Collectors.toList())));
+        LinkedHashMap<Integer, List<String>> namesMap = names.stream()
+                .collect(Collectors.groupingBy(String::length, LinkedHashMap::new, Collectors.filtering(s -> s.contains("e"), Collectors.toList())));
         System.out.println("namesMap = " + namesMap);
         System.out.println("-------------");
 
 
         // instructor grouping them by senior(>10) and junior(<10) and filter them on online courses
-        Map<String, List<Instructor>> mapOnlineCoursesInstructor = Instructors.getAll().stream()
+        LinkedHashMap<String, List<Instructor>> mapOnlineCoursesInstructor = Instructors.getAll().stream()
                 .collect(Collectors.groupingBy(instructor -> instructor.getYearsOfExperience() > 10 ? "SENIOR" : "JUNIOR"
-                        , Collectors.filtering(Instructor::isOnlineCourse, Collectors.toList())));
+                        , LinkedHashMap::new, Collectors.filtering(Instructor::isOnlineCourse, Collectors.toList())));
 
         System.out.println("mapOnlineCoursesInstructor = " + mapOnlineCoursesInstructor);
         System.out.println("-------------");
