f02e2f53efe891cb90d7dd7675e57ad833e7b1f3
Atif <iamatif96@gmail.com>
2024-03-09 03:06:59 +0800

    consumer practice

--- src/com/modernJava/funcProgramming/ConsumerExample3.java
+++ src/com/modernJava/funcProgramming/ConsumerExample3.java
@@ -9,13 +9,16 @@ public class ConsumerExample3 {
 
     public static void main(String[] args) {
 
-        IntConsumer intConsumer = a -> System.out.println(a*10);
+        IntConsumer intConsumer = a -> System.out.println(a * 10);
         intConsumer.accept(10);
 
-        LongConsumer longConsumer = a-> System.out.println(a* 10L);
+        LongConsumer longConsumer = a -> System.out.println(a * 10L);
         longConsumer.accept(10L);
 
-        DoubleConsumer doubleConsumer = a -> System.out.println(a*100);
+        DoubleConsumer doubleConsumer = a -> System.out.println(a * 100);
         doubleConsumer.accept(10000);
+
+        Consumer<String> consumer = System.out::println;
+        consumer.accept("Atif");
     }
 }
