b71e25b528298b467090c3c321ce90f707da6b8b
Atif <iamatif96@gmail.com>
2024-03-10 12:29:56 +0800

    consumer description

--- src/com/modernJava/funcProgramming/ConsumerExample.java
+++ src/com/modernJava/funcProgramming/ConsumerExample.java
@@ -5,6 +5,7 @@ import java.util.function.Consumer;
 public class ConsumerExample {
 //    It will consume one argument and return nothing, have one method accept(),
 //    package : java.util.function.Consumer
+//    Consumer<String> it will accept String type
     public static void main(String[] args) {
 
         Consumer<String> consumer = x -> System.out.println(x + " value of x:  " + x);
