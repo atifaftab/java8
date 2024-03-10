ee3560bb38046fa14f2386b1b1abcb929d89b148
Atif <iamatif96@gmail.com>
2024-03-10 12:05:35 +0800

    Predicate practice

--- src/com/modernJava/funcProgramming/PredicateExample.java
+++ src/com/modernJava/funcProgramming/PredicateExample.java
@@ -6,14 +6,14 @@ public class PredicateExample {
     public static void main(String[] args) {
 
         //if number>10 return tre or false
-        Predicate<Integer> predicate = i -> i>10;
+        Predicate<Integer> predicate = i -> i > 10;
         System.out.println(predicate.test(14));
         System.out.println(predicate.test(5));
 
         System.out.println("---------------");
 
         //i>10 && number is even number (i%2==0)
-        Predicate<Integer> predicate1 = i -> i%2==0;
+        Predicate<Integer> predicate1 = i -> i % 2 == 0;
         System.out.println(predicate.and(predicate1).test(16));
 
         System.out.println("---------------");
@@ -25,5 +25,12 @@ public class PredicateExample {
 
         //i>10 && number is odd (i%2!=0)
         System.out.println(predicate.and(predicate1.negate()).test(12));
+
+        System.out.println("---------------");
+
+
+        Predicate<Integer> predicate2 = i -> i > 5;
+        Predicate<Integer> predicate3 = i -> i < 8;
+        System.out.println(predicate2.and(predicate3).and(predicate1.negate()).test(7));
     }
 }
