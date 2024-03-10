60c87806e6805e0b412006b1daeb61162cb1361c
Atif <iamatif96@gmail.com>
2024-03-10 12:20:08 +0800

    comments in consumer

--- src/com/modernJava/funcProgramming/ConsumerExample.java
+++ src/com/modernJava/funcProgramming/ConsumerExample.java
@@ -3,7 +3,10 @@ package com.modernJava.funcProgramming;
 import java.util.function.Consumer;
 
 public class ConsumerExample {
+//    It will consume one argument and return nothing, have one method accept(),
+//    package : java.util.function.Consumer
     public static void main(String[] args) {
+
         Consumer<String> consumer = x -> System.out.println(x + " value of x:  " + x);
         consumer.accept("Hello Mr programmer ? How's your life ? ");
     }
