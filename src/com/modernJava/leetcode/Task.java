353333e4b700270614beae683e59fff8b57d65b9
Atif <iamatif96@gmail.com>
2024-03-31 21:16:09 +0800

    misc and task

---
+++ src/com/modernJava/leetcode/Task.java
@@ -0,0 +1,41 @@
+package com.modernJava.leetcode;
+
+public class Task {
+
+    private Long id;
+    private String description;
+    private Long priority;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Long priority) {
+        this.priority = priority;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", priority=" + priority +
+                '}';
+    }
+}
