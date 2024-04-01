f0729ccab5871976bf37d3cca9c41ab1976d4c8c
Atif <iamatif96@gmail.com>
2024-04-01 11:53:20 +0800

    maximum experience

--- src/com/modernJava/streams/StreamReduceExample2.java
+++ src/com/modernJava/streams/StreamReduceExample2.java
@@ -3,6 +3,7 @@ package com.modernJava.streams;
 import com.modernJava.funcProgramming.Instructor;
 import com.modernJava.funcProgramming.Instructors;
 
+import java.util.Comparator;
 import java.util.Optional;
 
 public class StreamReduceExample2 {
@@ -11,6 +12,18 @@ public class StreamReduceExample2 {
         Optional<Instructor> yearsOfExperienceOptional = Instructors.getAll().stream()
                 .reduce((a, b) -> a.getYearsOfExperience() > b.getYearsOfExperience() ? a : b);
         yearsOfExperienceOptional.ifPresent(System.out::println);
+
+
+        System.out.println("-----");
+        Optional<Integer> max = Instructors.getAll().stream()
+                .map(Instructor::getYearsOfExperience)
+                .max(Comparator.comparing(Integer::intValue));
+        max.ifPresent(System.out::println);
+
+        Optional<Integer> max1 = Instructors.getAll().stream()
+                .map(Instructor::getYearsOfExperience)
+                .max((a, b) -> a > b ? a : b);
+        System.out.println(max1.get());
 
     }
 }
