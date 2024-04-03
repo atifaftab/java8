8d03a53a4a9d687ae31129bacf50b4bfc1a69be7
Atif <iamatif96@gmail.com>
2024-04-03 18:10:41 +0800

    achieving same thing with normal method

--- src/com/modernJava/streams/GroupingExample2.java
+++ src/com/modernJava/streams/GroupingExample2.java
@@ -3,6 +3,7 @@ package com.modernJava.streams;
 import com.modernJava.funcProgramming.Instructor;
 import com.modernJava.funcProgramming.Instructors;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -13,6 +14,11 @@ public class GroupingExample2 {
         //grouping by length of String and also checking that the names contains e and only return those names
         // which has e in it
         List<String> names = List.of("Sid", "Mike", "Jenny", "Gene", "Rajeev");
+
+        //same thing with normal method
+        System.out.println(names.stream().filter(s -> s.contains("e")).collect(Collectors.groupingBy(String::length)));
+        System.out.println("-------------");
+
         Map<Integer, List<String>> namesMap = names.stream()
                 .collect(Collectors.groupingBy(String::length, Collectors.filtering(s -> s.contains("e"), Collectors.toList())));
         System.out.println("namesMap = " + namesMap);
@@ -20,6 +26,12 @@ public class GroupingExample2 {
 
 
         // instructor grouping them by senior(>10) and junior(<10) and filter them on online courses
+        System.out.println(Instructors.getAll().stream()
+                .filter(Instructor::isOnlineCourse)
+                .collect(Collectors.groupingBy(i -> i.getYearsOfExperience() > 10 ? "SENIOR" : "JUNIOR")));
+
+        System.out.println("-------------");
+
         Map<String, List<Instructor>> mapOnlineCoursesInstructor = Instructors.getAll().stream()
                 .collect(Collectors.groupingBy(instructor -> instructor.getYearsOfExperience() > 10 ? "SENIOR" : "JUNIOR"
                         , Collectors.filtering(Instructor::isOnlineCourse, Collectors.toList())));
