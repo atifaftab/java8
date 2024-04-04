1fd9ffd1cc65a749ec26ce2f1f99e59e08e45db9
Atif <iamatif96@gmail.com>
2024-04-04 09:29:54 +0800

    optional ifpresent and ofnullable

--- src/com/modernJava/optional/OptionalOfExample.java
+++ src/com/modernJava/optional/OptionalOfExample.java
@@ -7,5 +7,15 @@ public class OptionalOfExample {
         Optional<String> optionalString = Optional.of("Hey there");
 //        Optional<String> optionalString = Optional.of(null);
         System.out.println(optionalString);
+
+        System.out.println(getWords());
+    }
+
+    public static Optional<String> getWords() {
+        String[] words = new String[10];
+        Optional<String> word = Optional.ofNullable(words[2]);
+        if (word.isPresent())
+            return word;
+        return Optional.empty();
     }
 }
