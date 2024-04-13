a73b9c15c8f5d4f3242eb3c378851ffc729fa1b2
Atif <iamatif96@gmail.com>
2024-04-13 22:09:09 +0800

    default static methods of List interface

--- src/com/modernJava/defaults/DefaultsStaticExample.java
+++ src/com/modernJava/defaults/DefaultsStaticExample.java
@@ -2,12 +2,17 @@ package com.modernJava.defaults;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class DefaultsStaticExample {
     public static void main(String[] args) {
-        List<String> names = Arrays.asList("Mike", "Bob", "Salmon", "calf", "monkey");
-        Collections.sort(names);
-        names.forEach(System.out::println);
+        List<String> names = Arrays.asList("Mike", "Bob", "Salmon", "Calf", "Monkey");
+//        Collections.sort(names);
+//        names.forEach(System.out::println);
+
+//        names.sort(Comparator.comparing(String::length));
+        names.sort(Comparator.naturalOrder());
+        System.out.println(names);
     }
 }
