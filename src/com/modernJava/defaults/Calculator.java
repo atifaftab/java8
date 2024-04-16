36e13bd34df28bb087116a6753b6a110d368d7f0
Atif <iamatif96@gmail.com>
2024-04-16 21:41:26 +0800

    Functional Interface with default static methods

---
+++ src/com/modernJava/defaults/Calculator.java
@@ -0,0 +1,17 @@
+package com.modernJava.defaults;
+
+@FunctionalInterface
+public interface Calculator {
+    int sum(int num1, int num2);
+
+    //we can not write 2nd abstract method as this is Functional interface
+//    int divide(int num1, int num2);
+
+    default int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    static int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+}
