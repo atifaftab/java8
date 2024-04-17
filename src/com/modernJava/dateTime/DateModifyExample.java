9a302b502e94e690e33b3da1a2e1643fe6233a18
Atif <iamatif96@gmail.com>
2024-04-17 14:44:45 +0800

    localDateTime

--- src/com/modernJava/dateTime/DateModifyExample.java
+++ src/com/modernJava/dateTime/DateModifyExample.java
@@ -66,7 +66,5 @@ public class DateModifyExample {
 
         LocalDateTime localDateTime1 = LocalDateTime.of(LocalDate.now(), LocalTime.now());
         System.out.println(localDateTime1);
-
-        //Duration
     }
 }
