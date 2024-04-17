d3f00fe20b076574504c1f08d51bdc3ceaff2be1
Atif <iamatif96@gmail.com>
2024-04-17 08:46:32 +0800

    old date and time

---
+++ src/com/modernJava/dateTime/DateTime.java
@@ -0,0 +1,17 @@
+package com.modernJava.dateTime;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateTime {
+    public static void main(String[] args) {
+        //old api
+        Date date = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yy");
+        System.out.println(dateFormat.format(date));
+
+        dateFormat = new SimpleDateFormat("HH-mm-ss");
+        System.out.println(dateFormat.format(date));
+    }
+}
