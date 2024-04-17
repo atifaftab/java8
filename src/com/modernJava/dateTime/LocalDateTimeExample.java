702b54ebe1e5140e8fd9df7f933a424a41bb29cc
Atif <iamatif96@gmail.com>
2024-04-17 18:35:09 +0800

    modifying localDateTime using Duration

--- src/com/modernJava/dateTime/LocalDateTimeExample.java
+++ src/com/modernJava/dateTime/LocalDateTimeExample.java
@@ -1,5 +1,6 @@
 package com.modernJava.dateTime;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -16,6 +17,9 @@ public class LocalDateTimeExample {
         LocalDateTime localDateTime1 = LocalDateTime.of(LocalDate.now(), LocalTime.now());
         System.out.println(localDateTime1);
 
-
+        LocalTime localTime = LocalTime.now();
+        LocalTime localTime1 = LocalTime.now().plusHours(4);
+        Duration duration = Duration.between(localTime, localTime1);
+        System.out.println("duration: " + duration.toMinutes());
     }
 }
