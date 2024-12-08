03838dbba415d6757fce0dbe618179299cc36a6a
Atif <iamatif96@gmail.com>
2024-12-08 10:24:03 +0800

    Functions: Consumer

--- src/com/modernJava/funcProgramming/ConsumerExample.java
+++ src/com/modernJava/funcProgramming/ConsumerExample.java
@@ -3,12 +3,15 @@ package com.modernJava.funcProgramming;
 import java.util.function.Consumer;
 
 public class ConsumerExample {
-//    It will consume one argument and return nothing, have one method accept(),
+    //    It will consume one argument and return nothing, have one method accept(),
 //    package : java.util.function.Consumer
 //    Consumer<String> it will accept String type
     public static void main(String[] args) {
 
         Consumer<String> consumer = x -> System.out.println(x + " value of x:  " + x);
         consumer.accept("Hello Mr programmer ? How's your life ? ");
+
+        Consumer<Integer> consumerInt = i -> System.out.println(i);
+        consumerInt.accept(10);
     }
 }
--- src/com/modernJava/funcProgramming/ConsumerExampleForInstructor.java
+++ src/com/modernJava/funcProgramming/ConsumerExampleForInstructor.java
@@ -8,80 +8,87 @@ public class ConsumerExampleForInstructor {
     public static void main(String[] args) {
 
 
-        List<Instructor> instructor = Instructors.getAll();
+        List<Instructor> instructors = Instructors.getAll();
 
-        //looping through all instructor and printing all values of instructor
+        //looping through all instructors and printing all values of instructors
         Consumer<Instructor> consumer = i -> System.out.println(i);
 //      Consumer<Instructor> consumer = System.out::println;
-        instructor.forEach(consumer);
+        instructors.forEach(consumer);
         System.out.println("--------------");
 
         //practice
-        instructor.forEach(System.out::println);
+        instructors.forEach(System.out::println);
         System.out.println("--------------");
 
-        //looping through all instructor and printing only name of all instructor
+        //looping through all instructors and printing only name of all instructors
         Consumer<Instructor> consumer1 = i -> System.out.println(i.getName());
-        instructor.forEach(consumer1);
-
-        System.out.println("--------------practice");
-
-        //practice
-        Consumer<Instructor> instructorConsumer = instructor1 -> System.out.println(instructor1.getName());
-        instructor.forEach(instructorConsumer);
+        instructors.forEach(consumer1);
 
         System.out.println("------------practice using directly stream and map------------");
         Instructors.getAll().stream().map(Instructor::getName).forEach(System.out::println);
 
-        System.out.println("--------------");
+        System.out.println("-------name and courses-------");
 
-        //looping through all instructor and printing the name and courses
+        //looping through all instructors and printing the name and courses
+        Instructors.getAll().stream().forEach(i -> {
+            System.out.println(i.getName());
+            System.out.println(i.getCourses());
+        });
+        System.out.println("-------name and course 2-------");
+
         Consumer<Instructor> consumer2 = i -> System.out.println(i.getCourses());
-        instructor.forEach(consumer1.andThen(consumer2));
+        instructors.forEach(consumer1.andThen(consumer2));
 
-        System.out.println("--------------");
+        System.out.println("------printing their name if the years of experience is >10--------");
 
-        //looping through all instructor and printing their name if the years of experience is >10
+
+        //looping through all instructors and printing their name if the years of experience is >10
 //        Consumer<Instructor> consumer3 = i -> {
 //            if (i.getYearsOfExperience() > 10)
 //                System.out.println(i.getName());
 //        };
-        instructor.forEach(i -> {
+        instructors.forEach(i -> {
             if (i.yearsOfExperience > 10) System.out.println(i.getName());
         });
 
-        //practice
-        System.out.println("--------------practice");
-        instructor.forEach(i -> {
-            if (i.getYearsOfExperience() > 10) {
-                System.out.println(i.getName());
-            }
-        });
+        //same without using if
+        instructors.stream().
+                filter(i -> i.getYearsOfExperience() > 10)
+//                .map(Instructor::getName)
+                .forEach(i -> System.out.println(i.getName()));
 
-        System.out.println("--------------");
+        System.out.println("------names and years of experiences  if the years of\n" +
+                "//         experience >5 and teaches course online--------");
 
 //        looping through all instructors and print out their names and years of experiences  if the years of
 //         experience >5 and teaches course online
-        instructor.forEach(i -> {
+        instructors.forEach(i -> {
             if (i.yearsOfExperience > 5 && (i.isOnlineCourse())) {
                 consumer1.andThen(j -> System.out.println(j.yearsOfExperience)).accept(i);
             }
         });
 
+        instructors.stream()
+                .filter(i -> i.getYearsOfExperience() > 5 && i.isOnlineCourse())
+                .forEach(i -> {
+                    System.out.println(i.getName());
+                    System.out.println(i.getYearsOfExperience());
+                });
 
         //test
-        instructor.forEach(i -> {
+        instructors.forEach(i -> {
             if (i.getYearsOfExperience() > 5 && i.isOnlineCourse()) {
                 consumer1.andThen(j -> System.out.println(j.yearsOfExperience)).accept(i);
             }
         });
-        System.out.println("--------------");
+
+        System.out.println("-------print name of only max experience-------");
 
         //print name of only max experience
-        Optional<Integer> maxExp = instructor.stream().map(Instructor::getYearsOfExperience).max(Integer::compareTo);
+        Optional<Integer> maxExp = instructors.stream().map(Instructor::getYearsOfExperience).max(Integer::compareTo);
         maxExp.ifPresent(System.out::println);
         System.out.println("--------------");
-        instructor.forEach(i -> {
+        instructors.forEach(i -> {
             if (maxExp.isPresent()) {
                 if (i.getYearsOfExperience() == maxExp.get()) {
                     System.out.println(i.getName());
@@ -89,16 +96,9 @@ public class ConsumerExampleForInstructor {
             }
         });
 
-        Optional<Integer> max = instructor.stream().map(Instructor::getYearsOfExperience).max(Integer::compareTo);
-        max.ifPresent(System.out::println);
+//        Instructors.getAll().stream()
+//                .filter(instructor -> instructor.getYearsOfExperience())                                                                                             )
 
-        Optional<Integer> maxExp1 = Instructors.getAll().stream().map(Instructor::getYearsOfExperience).max(Integer::compareTo);
-        maxExp1.ifPresent(System.out::println);
-        Instructors.getAll().forEach(i -> {
-            if (maxExp1.isPresent()) {
-                if (i.getYearsOfExperience() == maxExp1.get()) System.out.println(i.getName());
-            }
-        });
 
     }
 }
