9a84a465960a94418a3d5b7393d65915f6d11f7e
Atif <iamatif96@gmail.com>
2024-12-08 12:14:26 +0800

    Functions: Predicate, replacing if by filter

--- src/com/modernJava/funcProgramming/PredicateAndBiConsumerExample.java
+++ src/com/modernJava/funcProgramming/PredicateAndBiConsumerExample.java
@@ -12,16 +12,32 @@ public class PredicateAndBiConsumerExample {
         //all instructor who teaches online
         Predicate<Instructor> instructorPredicate = i -> i.isOnlineCourse();
 //        Predicate<Instructor> instructorPredicate = Instructor::isOnlineCourse;
-        //instructor experience is 10>10
+        //instructor experience is >10
         Predicate<Instructor> instructorPredicate1 = i -> i.getYearsOfExperience() > 10;
         //biConsumer print name and courses
         BiConsumer<String, List<String>> stringListBiConsumer = (name, courses) -> System.out.println("name " + name + " and courses " + courses);
 
-        //loop instructor and print
+        //loop instructor, check predicates and print consume
         instructorList.forEach(i -> {
             if (instructorPredicate.and(instructorPredicate1).test(i)) {
                 stringListBiConsumer.accept(i.getName(), i.getCourses());
             }
         });
+
+        //replacing if by filter
+        System.out.println("\n----- replacing if by filter -------");
+        instructorList.stream()
+                .filter(i -> instructorPredicate1.and(instructorPredicate).test(i))
+                .forEach(i -> stringListBiConsumer.accept(i.getName(), i.getCourses()));
+
+
+        System.out.println("\n----- doing same without predicate and consumer");
+        instructorList.stream()
+                .filter(i -> i.getYearsOfExperience() > 10 && i.isOnlineCourse())
+                .forEach(i -> {
+                    System.out.print("name " + i.getName());
+                    System.out.println(" and courses " + i.getCourses());
+                });
+
     }
 }
