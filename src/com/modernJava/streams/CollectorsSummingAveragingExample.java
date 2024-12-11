b4299c34ae74fc4e7798e6b71bb11f30c2a0c6ac
Atif <iamatif96@gmail.com>
2024-12-11 18:25:31 +0800

    Collections.summingInt

--- src/com/modernJava/streams/CollectorsSummingAveragingExample.java
+++ src/com/modernJava/streams/CollectorsSummingAveragingExample.java
@@ -8,6 +8,22 @@ import java.util.stream.Collectors;
 public class CollectorsSummingAveragingExample {
     public static void main(String[] args) {
         //sum of years of experience of all Instructor
+        System.out.println("SUM:");
+        System.out.println(Instructors.getAll().stream()
+                .map(Instructor::getYearsOfExperience)
+                .reduce(Integer::sum).get());
+
+        System.out.println(Instructors.getAll().stream()
+                .mapToInt(Instructor::getYearsOfExperience)
+                .sum());
+
+        System.out.println(Instructors.getAll().stream()
+                .map(Instructor::getYearsOfExperience)
+                .mapToInt(Integer::intValue)
+                .sum());
+
+        System.out.println("----");
+
         int sum = Instructors.getAll().stream()
                 .collect(Collectors.summingInt(Instructor::getYearsOfExperience));
 //                .mapToInt(Instructor::getYearsOfExperience).sum();
